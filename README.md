9c18ce9e888b5e5eb790b869a1e7c3f051e82739
Krushovice <kickstar69@yandex.ru>
2023-10-15 06:29:55 +0000

    fix env

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Krushovice/python-project-83/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Krushovice/python-project-83/actions)
+
+## Link on web-service: https://krushovice-page-anylyzer.onrender.com/
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/krushovice/study-projects/hexlet-python-project-83/venv"
+set -gx VIRTUAL_ENV "/home/krushovice/study-projects/python-project-83/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
